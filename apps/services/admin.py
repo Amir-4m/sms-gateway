24fdcb016f41f0d544a0dcec5f650f11a0ac9459
amir <amiri.amir30@yahoo.com>
2020-09-16 16:35:30 +0430

    service admin updated

--- apps/services/admin.py
+++ apps/services/admin.py
@@ -1,4 +1,8 @@
+import secrets
+import string
+
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 from apps.services.models import Service
 
@@ -7,3 +11,16 @@ from apps.services.models import Service
 class ServiceModelAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'is_enable', 'created_time', 'updated_time')
     list_filter = ('is_enable',)
+    readonly_fields = ('secret_key',)
+
+    change_form_template = "services/admin/change-form.html"
+
+    def random_secret_generator(self, length):
+        return ''.join(secrets.choice(string.ascii_uppercase + string.digits) for i in range(length))
+
+    def response_change(self, request, obj):
+        if "change-secret" in request.POST:
+            obj.secret_key = self.random_secret_generator(10)
+            obj.save()
+            return HttpResponseRedirect(".")  # stay on the same detail page
+        return super().response_change(request, obj)
