1cd8217a4f91dac36ae168b5f652be6dd7a87d77
amir <amiri.amir30@yahoo.com>
2020-10-11 16:36:32 +0330

    api permission updated

--- apps/services/api/permissions.py
+++ apps/services/api/permissions.py
@@ -3,4 +3,7 @@ from rest_framework.permissions import BasePermission
 
 class ServicePermission(BasePermission):
     def has_permission(self, request, view):
-        return 'service' in request.auth
+        if request.auth is None:
+            return False
+
+        return 'service' in request.auth and request.auth['service'].is_enable
